2ef6ec90f923d8ff9664f847667ef205c0633133
vivek <vivekvasu1515@gmail.com>
2024-06-03 13:43:27 +0530

    Revert "Initial Commit"
    This reverts commit bbf5f663a3d30514dbc085b604e8111d81ca846b.

--- employee-system-api/src/main/java/org/example/secureapp/employeesystemapi/Repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.secureapp.employeesystemapi.Repository;
-
-import org.example.secureapp.employeesystemapi.Model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-}
